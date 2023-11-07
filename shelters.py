fe66a86cfb812d51930b265c571c71e291f6ad78
Bileam Scheuvens <36153336+bileamScheuvens@users.noreply.github.com>
2023-11-07 19:15:33 +0100

    Add HN, K, SB, LU

--- shelters.py
+++ shelters.py
@@ -76,3 +76,43 @@ class StuttgartShelter(AbstractShelter):
             cat.h2.a.get_text(): cat.find("img")["src"]
             for cat in self.soup.find(class_="table_tiere_left_td")
         }
+
+class HNShelter(AbstractShelter):
+    def __init__(self):
+        super().__init__("https://www.heilbronner-tierschutz.de/tiervermittlung/katzen/", "Tierschutzverein Heilbronn")
+
+    def get_cats(self) -> dict:
+        return {cat.a.get("title"): cat.a.img.get("src") for cat in self.soup.find_all(class_="qode-post-image")}
+    
+class KoelnShelter(AbstractShelter):
+    def __init__(self):
+        super().__init__("https://tierheim-koeln-dellbrueck.de/vermittlung/katzen", "Tierheim Koeln Dellbrueck")
+    
+    def clean_name(self, name):
+        name = name.split("(")[0]
+        return name.strip()
+
+    def get_cats(self) -> dict:
+        return {self.clean_name(cat.img.get("alt")): cat.img.get("src") for cat in self.soup.find_all("figure")}
+
+class SBShelter(AbstractShelter):
+    def __init__(self):
+        super().__init__("https://www.tierheim-saarbruecken.de/zuhause-gesucht/katzen", "Tierheim Saarbruecken")
+    
+    def clean_name(self, name):
+        name = name.replace("\n", "")
+        name = name.replace("\t", "")
+        name = name.replace("!!! Notfall !!!", "")
+        return name.strip()
+
+    def get_cats(self) -> dict:
+        names = [self.clean_name(cat.get_text()) for cat in self.soup.find_all("h2")]
+        imgs = ["https://www.tierheim-saarbruecken.de/"+cat.img.get("src") for cat in self.soup.find_all(class_="item active")]
+        return dict(zip(names, imgs))
+    
+class LUShelter(AbstractShelter):
+    def __init__(self):
+        super().__init__("https://tierheim-ludwigshafen.com/katzen/", "Tierheim Ludwigshafen")
+
+    def get_cats(self) -> dict:
+        return {cat.img.get("alt"): cat.img.get("src") for cat in self.soup.find_all(class_="et_portfolio_image")}
